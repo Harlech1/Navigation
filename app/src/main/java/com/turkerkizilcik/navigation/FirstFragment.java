1815933f25e03cf8669f5c434a47c03d3f83d158
Harlech <turkerkizilcik@gmail.com>
2022-11-17 00:40:41 +0300

    Initial commit

---
+++ app/src/main/java/com/turkerkizilcik/navigation/FirstFragment.java
@@ -0,0 +1,57 @@
+package com.turkerkizilcik.navigation;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class FirstFragment extends Fragment {
+
+
+
+    public FirstFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSecond(view);
+            }
+        });
+    }
+
+    public void goToSecond (View view) {
+        NavDirections action = FirstFragmentDirections.actionFirstFragmentToSecondFragment();
+        Navigation.findNavController(view).navigate(action);
+    }
+
+
+}
